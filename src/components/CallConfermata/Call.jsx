934ddf65d94021bcc5855ac79163567c7b395d72
StefanoWork <stefanom87.work@outlook.it>
2024-12-09 19:12:32 +0100

    Aggiunto freccia per chiamata confermata

--- src/components/CallConfermata/Call.jsx
+++ src/components/CallConfermata/Call.jsx
@@ -2,6 +2,8 @@ import style from "./Call.module.css";
 import Image from "next/image";
 
 import Bolla from "../../Images/Bolla.png";
+import Arrow from "../../Images/ArrowC.png";
+import Regalo from "../../Images/ArrowR.png";
 
 export default function Call() {
   return (
@@ -11,14 +13,20 @@ export default function Call() {
         Riceverai un&apos;email all&apos;indirizzo indicato con tutti i dati
       </h2>
 
-      <h3 className={`${style.regalo} barlow`}>
-        Guarda questo breve video per prepararti alla tua call
+      <h3 className={`${style.regalo} barlow ${style.fadeColor}`}>
+        Durante la call riceverai dei consigli gratuiti del tuo sito web o 
+        del tuo profilo social per migliorare 
+        subito la tua presenza online. 
       </h3>
       <iframe
         src="https://fast.wistia.com/embed/medias/lw5lhial8n"
         title="Video"
         className={style.video}
       ></iframe>
+
+      <Image src={Arrow} alt="Freccia" className={style.freccia} />
+      {/* <Image src={Regalo} alt="Freccia" className={style.freccia2} /> */}
+      
 
       <Image src={Bolla} alt="" className={style.bolla} />
       <Image src={Bolla} alt="" className={style.bolla2} />
