488373ca60405d38d23b622e43fbafc74a1a8b7f
StefanoWork <stefanom87.work@outlook.it>
2024-12-15 17:07:52 +0100

    Modificato layout landing

--- src/components/PageRegaloDesign/Video1/Video1.jsx
+++ src/components/PageRegaloDesign/Video1/Video1.jsx
@@ -7,7 +7,7 @@ import Bolla from "../../../Images/Bolla.png";
 export default function Video1() {
     return (
          <div className={style.container} id="Method">
-            <h2 className={` ${style.title} mulish`}>La teoria del<br/> Web Design</h2>
+            <h2 className={` ${style.title} mulish`}>Come creare un<br/> Web Design</h2>
             <iframe className={style.video} src="https://fast.wistia.net/embed/iframe/7camipfw73?seo=false&videoFoam=true" title="Come crescere con un sito web" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; fullscreen" allowfullscreen></iframe>
 
             <Image className={style.bolla1} src={Bolla} alt="" />
