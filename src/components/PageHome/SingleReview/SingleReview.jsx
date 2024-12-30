d291fc18877abee739ae7435ea5d2eb1dd75ec2c
StefanoWork <stefanom87.work@outlook.it>
2024-12-30 09:55:34 +0100

    Some change

--- src/components/PageHome/SingleReview/SingleReview.jsx
+++ src/components/PageHome/SingleReview/SingleReview.jsx
@@ -13,12 +13,14 @@ export default function SingleReview({
   };
 
   return (
-    <div className={`${style.review} ${isVideoLink ? style.hasVideo : style.noVideo}`}>
+    <div className={`${style.review} ${isVideoLink(Media) ? style.hasVideo : style.noVideo}`}>
       <div className={style.Sx}>
         {isVideoLink(Media) ? (
           <iframe className={style.media} src={Media} />
         ) : (
-          <Image src={Media} alt="Image" className={style.image} width={100} height={800} />
+          <div className={style.imagecontainer}>
+            <Image src={Media} alt="Person" className={style.image} width={100} height={800} />
+          </div>
         )}
       </div>
       <div className={style.Dx}>
@@ -27,7 +29,6 @@ export default function SingleReview({
           <p className={`${style.name} barlow`}>{Nome}</p>
         </div>
         <p className={`${style.text} barlow`}>{Testo}</p>
-      
       </div>
     </div>
   );
